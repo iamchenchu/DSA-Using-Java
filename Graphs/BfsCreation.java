fb14ad54a8928f8d6ab74557f918b24fc47ee179
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-08 09:25:15 -0600

    this is a code for BFS creation with an example

--- Graphs/BfsCreation.java
+++ Graphs/BfsCreation.java
@@ -1,6 +1,9 @@
 package Graphs;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.*;
 
 public class BfsCreation {
 
@@ -41,14 +44,38 @@ public class BfsCreation {
             graph[5].add(new Edge(5, 4, 1));
             graph[5].add(new Edge(5, 6, 1));
 
-            graph[5].add(new Edge(6, 5, 0));
+            graph[6].add(new Edge(6, 5, 1));
 
+        }
+        public static void bfs(ArrayList<Edge>[] graph)
+        {
+            Queue<Integer> q = new LinkedList<>();
+            boolean vis[] = new boolean[graph.length];
+            q.add(0); // source = 0
+
+            while(!q.isEmpty())
+            {
+                int curr = q.remove();
+                if(!vis[curr])
+                { // visit currr
+                    System.out.print(curr+" ");
+                    vis[curr] = true;
+                    for(int i =0; i<graph[curr].size(); i++)
+                    {
+                        Edge e = graph[curr].get(i);
+                        q.add(e.dest);
+                    }
+
+
+                }
+            }
         }
 
         public static void main(String[] args) {
             int V = 7;
             ArrayList<Edge> graph[] = new ArrayList[V];
             createGraph(graph);
+            bfs(graph);
         }
 
     }
