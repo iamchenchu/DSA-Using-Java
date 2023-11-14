0a33cfd9b5e2f8137267b60860c6603ad750b8e0
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-13 20:03:53 -0600

    added code for cycle detection in graphs using DFS

---
+++ Graphs/CycleDetectionDFS.java
@@ -0,0 +1,75 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class CycleDetectionDFS {
+
+    public class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+
+        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 3));
+    }
+
+    public static boolean detectCycle(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if (detectCycleUtil(graph, vis, i, -1)) {
+                    return true; // cycle exists in one of the parts
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean vis[], int curr, int par) {
+        vis[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            // case3
+            if (!vis[e.dest] && detectCycleUtil(graph, vis, e.dest, curr)) {
+                return true;
+            }
+            // case1
+            else if (vis[e.dest] && e.dest != par) {
+                return true;
+            }
+
+            // case2 -> do nothing -> continue
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        System.out.println(detectCycle(graph));
+    }
+
+}
--- Graphs/UndirectedAdjacencyList.java
+++ Graphs/UndirectedAdjacencyList.java
@@ -4,36 +4,31 @@ import java.util.LinkedList;
 
 public class UndirectedAdjacencyList {
 
-    private LinkedList<Integer>[] adj;   // array of linkedlist 
+    private LinkedList<Integer>[] adj; // array of linkedlist
     private int V; // number of vertices
-    private int E;  // number of edges
+    private int E; // number of edges
 
-    public UndirectedAdjacencyList(int nodes)
-    {
+    public UndirectedAdjacencyList(int nodes) {
         this.V = nodes;
-        this.E =0;
+        this.E = 0;
         this.adj = new LinkedList[nodes];
-        for(int v =0; v<V;v++)
-        {
+        for (int v = 0; v < V; v++) {
             adj[v] = new LinkedList<>();
         }
-
     }
 
-    public void addEdge(int start, int dest)
-    {
+    public void addEdge(int start, int dest) {
         adj[start].add(dest);
         adj[dest].add(start);
         E++;
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(V + "Vertices, "+ E + "Edges"+ "\n");
-        for(int v=0; v<V; v++)
-        {
+        sb.append(V + "Vertices, " + E + "Edges" + "\n");
+        for (int v = 0; v < V; v++) {
             sb.append(v + ": ");
-            for(int w : adj[v]){
+            for (int w : adj[v]) {
                 sb.append(w + " ");
             }
             sb.append("\n");
@@ -42,23 +37,22 @@ public class UndirectedAdjacencyList {
         return sb.toString();
 
     }
-    public static void main(String[] args )
-    {
-        UndirectedAdjacencyList g =  new UndirectedAdjacencyList(4);
-        g.addEdge(0,1);
-        g.addEdge(1,2);
-        g.addEdge(2,3);
-        g.addEdge(3,0);
+
+    public static void main(String[] args) {
+        UndirectedAdjacencyList g = new UndirectedAdjacencyList(4);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
 
         System.out.println(g);
     }
-    
+
 }
 
-
-// output 
+// output
 // 4Vertices, 4Edges
-// 0: 1 3 
-// 1: 0 2 
-// 2: 1 3 
-// 3: 2 0 
+// 0: 1 3
+// 1: 0 2
+// 2: 1 3
+// 3: 2 0
