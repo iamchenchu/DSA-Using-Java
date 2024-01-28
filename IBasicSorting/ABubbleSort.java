e10ff3094c3b270c1738bee1a572dd337245e213
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-01-27 22:13:46 -0600

    Added the code

---
+++ IBasicSorting/ABubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ABubbleSort {
+
+    public static void bubbleSort(int[] arr){
+        for(int turn=0; turn<arr.length-1; turn++){
+
+            for(int j=0; j<arr.length-1-turn; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp =arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+
+            }
+        }
+
+    }
+
+    public static void displayArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {5, 4, 1, 3, 2};
+        bubbleSort(arr);
+        displayArray(arr);
+    }
+    
+}
